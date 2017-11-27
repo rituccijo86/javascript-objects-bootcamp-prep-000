4f1f87b861880720f9c1234bdaab0e925f5692dc
John R <john.l.ritucci@gmail.com>
2017-11-27 23:24:33 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {artist1: "song 1"};
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist.artistName = songTitle;
+  playlist.artistName = 'songTitle';
   return playlist
 
 }
